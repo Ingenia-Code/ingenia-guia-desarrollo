557220d4b190165892ad94e404c808576bca11e8
Armando Nava <armando.nava@ingenia.com>
2025-03-19 14:19:11 -0600

    update

--- js/navigation.js
+++ js/navigation.js
@@ -204,7 +204,7 @@ function showWelcomePage() {
             </div>
             <div class="row">
                 ${menuStructure.map(section => `
-                    <div class="col-xl-4 col-md-6 mb-4">
+                    <div class="col-xl-3 col-md-6 mb-3">
                         <div class="card border-left-primary shadow h-100 py-2">
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
